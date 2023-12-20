6d2f48e25e901a9eaf6b7a9e3daca2cc2a05b223
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-12-19 21:35:44 -0400

    Mapas 2

--- 20231213/Pages/MapaPage.xaml.cs
+++ 20231213/Pages/MapaPage.xaml.cs
@@ -1,4 +1,6 @@
+using Microsoft.Maui.Controls.Maps;
 using Microsoft.Maui.Maps;
+using System.Diagnostics;
 
 namespace _20231213.Pages;
 
@@ -11,5 +13,30 @@ public partial class MapaPage : ContentPage
 		//MapSpan mapSpan = new MapSpan(location, 0.01,0.01);
 		//mimapa.MoveToRegion(mapSpan);
 
+		mimapa.Pins.Add(new Microsoft.Maui.Controls.Maps.Pin { 
+			Label = "Pepe está en el colegio Don Bosco",
+			Location = new Location(-16.5034187368878, -68.13048092406169)
+		});
+
+		mimapa.MapType = MapType.Satellite;
     }
+
+	async void OnMapaClic(object sender, MapClickedEventArgs e)
+	{
+		Debug.WriteLine($"[MAPA] Clic: {e.Location.Latitude}, {e.Location.Longitude}");
+		Polygon recuadro = new Polygon
+		{
+			StrokeColor = Color.FromArgb("#FF0000"),
+			StrokeWidth = 8,
+			FillColor = Color.FromArgb("#8800AAFF"),
+			Geopath =
+			{
+				new Location(-16.500962176388644, -68.13296701536892),
+				new Location(-16.501136598993906, -68.13280569465373),
+				new Location(-16.50077294413901, -68.13236978548714),
+				new Location(-16.50061662170619, -68.13255856504747)
+			}
+		};
+		mimapa.MapElements.Add(recuadro);
+	}
 }
